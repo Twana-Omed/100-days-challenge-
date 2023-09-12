9b4a8ac8ff783afd42a0395e88dd2c6937f41b67
Twana Omed <112326906+Twana-Omed@users.noreply.github.com>
2023-09-12 20:35:48 +0300

    Add files via upload

---
+++ 12th day/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+// var generateName = require('sillyname');
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// import gerenateSuperhero from "superheros";
+// var superheros = gerenateSuperhero();
+import gerenateSuperhero from "superheros";
+const name = superhero.random();
+
+console.log(`my name is ${gerenateSuperhero} .`);
